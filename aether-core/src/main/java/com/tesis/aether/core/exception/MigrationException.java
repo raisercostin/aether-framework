df787f9bcd1dae4071490bbe0c4581858d762f0d
joaquinpf <joaquinpf@gmail.com>
2011-02-20 22:21:06 +0000

    Storage: Implementacion de migrate con sus tests. Ya esta la interface actual completa :). Se viene la etapa 3

---
+++ aether-core/src/main/java/com/tesis/aether/core/exception/MigrationException.java
@@ -0,0 +1,11 @@
+package com.tesis.aether.core.exception;
+
+public class MigrationException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public MigrationException(String message) {
+		super(message);
+	}
+
+}
