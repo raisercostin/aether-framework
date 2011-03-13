bb4e355cba4364720108dbf7d72ab75162e9f5bf
joaquinpf@gmail.com <joaquinpf@gmail.com@e3f461f3-df42-3739-7a70-0f39b822fab9>
2011-03-13 19:51:43 +0000

    nc

--- trunk/aether-core/src/test/java/com/tesis/aether/core/S3StorageServiceTest.java
+++ trunk/aether-core/src/test/java/com/tesis/aether/core/S3StorageServiceTest.java
@@ -2,21 +2,21 @@ package com.tesis.aether.core;
 
 import com.tesis.aether.core.factory.ServiceAccountProperties;
 import com.tesis.aether.core.services.storage.S3StorageService;
-import com.tesis.aether.core.services.storage.StorageService;
+import com.tesis.aether.core.services.storage.ExtendedStorageService;
 import com.tesis.aether.core.services.storage.constants.StorageServiceConstants;
 
 
 public class S3StorageServiceTest extends StorageServiceTest {
 
 	@Override
-	protected StorageService getStorageService() {
+	protected ExtendedStorageService getStorageService() {
 		ServiceAccountProperties properties = new ServiceAccountProperties();
 		properties.putProperty(StorageServiceConstants.LOCAL_BASE_FOLDER, "d:/REMOTE_MOCK/");
 		properties.putProperty(StorageServiceConstants.S3_ACCESS_KEY, "");
 		properties.putProperty(StorageServiceConstants.S3_SECRET_KEY, "");
 		properties.putProperty(StorageServiceConstants.S3_BUCKET, "");
-		
-		StorageService service = new S3StorageService();
+
+		ExtendedStorageService service = new S3StorageService();
 		service.setServiceProperties(properties);
 		
 		return service;
