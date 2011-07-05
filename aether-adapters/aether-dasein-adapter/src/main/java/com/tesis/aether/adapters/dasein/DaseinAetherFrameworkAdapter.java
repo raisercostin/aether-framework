9703ab3eb660d382cb06cc116096cfa520997957
joaquinpf <joaquinpf@gmail.com>
2011-07-05 22:36:34 +0000

    Mas metodos implementados en el adapter de Dasein

--- aether-adapters/aether-dasein-adapter/src/main/java/com/tesis/aether/adapters/dasein/DaseinAetherFrameworkAdapter.java
+++ aether-adapters/aether-dasein-adapter/src/main/java/com/tesis/aether/adapters/dasein/DaseinAetherFrameworkAdapter.java
@@ -12,8 +12,12 @@ import org.dasein.cloud.encryption.Encryption;
 import org.dasein.cloud.storage.CloudStoreObject;
 import org.dasein.cloud.storage.FileTransfer;
 
+import com.tesis.aether.core.exception.CreateContainerException;
+import com.tesis.aether.core.exception.DeleteContainerException;
+import com.tesis.aether.core.exception.DeleteException;
 import com.tesis.aether.core.exception.FileNotExistsException;
 import com.tesis.aether.core.exception.MethodNotSupportedException;
+import com.tesis.aether.core.exception.MoveFileException;
 import com.tesis.aether.core.exception.UploadException;
 import com.tesis.aether.core.framework.adapter.AetherFrameworkAdapter;
 import com.tesis.aether.core.services.storage.object.StorageObjectMetadata;
@@ -98,29 +102,89 @@ public class DaseinAetherFrameworkAdapter extends AetherFrameworkAdapter {
 		}
 	}
 
-	/**
-	 * UNIMPLEMENTED METHODS
-	 */
-
 	public void clear(String directory) throws CloudException, InternalException {
-		// TODO Auto-generated method stub
+
+		try {
+			service.delete(directory, "", true);
+		} catch (DeleteException e) {
+			e.printStackTrace();
+			throw new InternalException(e.getMessage());
+		}
 
 	}
 
 	public String createDirectory(String baseName, boolean findFreeName) throws InternalException, CloudException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			service.createContainer(baseName);
+			return baseName;
+		} catch (CreateContainerException e) {
+			e.printStackTrace();
+			throw new InternalException(e.getMessage());
+		}
 	}
 
 	public boolean exists(String directory) throws InternalException, CloudException {
-		// TODO Auto-generated method stub
-		return false;
+		return service.existsContainer(directory);
 	}
 
 	public long exists(String directory, String fileName, boolean multiPart) throws InternalException, CloudException {
-		// TODO Auto-generated method stub
-		return 0;
+		try {
+			if (service.checkObjectExists(directory, fileName)) {
+				return 0L;
+			} else {
+				return -1L;
+			}
+		} catch (MethodNotSupportedException e) {
+			e.printStackTrace();
+			return -1L;
+		}
 	}
+
+	public void moveFile(String fromDirectory, String fileName, String toDirectory) throws InternalException, CloudException {
+		try {
+			service.moveFile(fromDirectory, fileName, toDirectory, fileName);
+		} catch (MoveFileException e) {
+			e.printStackTrace();
+			throw new InternalException(e.getMessage());
+		}
+	}
+
+	public void removeDirectory(String directory) throws CloudException, InternalException {
+		try {
+			service.deleteContainer(directory);
+		} catch (DeleteContainerException e) {
+			e.printStackTrace();
+			throw new InternalException(e.getMessage());
+		}
+	}
+
+	public void removeFile(String directory, String name, boolean multipartFile) throws CloudException, InternalException {
+		try {
+			service.delete(directory, name, false);
+		} catch (DeleteException e) {
+			e.printStackTrace();
+			throw new InternalException(e.getMessage());
+		}
+	}
+
+	public void renameFile(String directory, String oldName, String newName) throws CloudException, InternalException {
+		try {
+			if (service.checkFileExists(directory, oldName)) {
+				service.moveFile(directory, oldName, directory, newName);
+			}
+		} catch (MethodNotSupportedException e) {
+			e.printStackTrace();
+			throw new InternalException(e.getMessage());
+		} catch (MoveFileException e) {
+			e.printStackTrace();
+			throw new InternalException(e.getMessage());
+		}
+
+	}
+
+	/**
+	 * UNIMPLEMENTED METHODS
+	 */
 
 	public long getMaxFileSizeInBytes() throws InternalException, CloudException {
 		// TODO Auto-generated method stub
@@ -152,29 +216,9 @@ public class DaseinAetherFrameworkAdapter extends AetherFrameworkAdapter {
 
 	}
 
-	public void moveFile(String fromDirectory, String fileName, String toDirectory) throws InternalException, CloudException {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void removeDirectory(String directory) throws CloudException, InternalException {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void removeFile(String directory, String name, boolean multipartFile) throws CloudException, InternalException {
-		// TODO Auto-generated method stub
-
-	}
-
 	public String renameDirectory(String oldName, String newName, boolean findFreeName) throws CloudException, InternalException {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	public void renameFile(String directory, String oldName, String newName) throws CloudException, InternalException {
-		// TODO Auto-generated method stub
-
 	}
 
 }
