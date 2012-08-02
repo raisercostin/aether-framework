bda55fe402c58dd93379d41d55fae077943b31f2
marcos_m_s <marcos_m_s@hotmail.com>
2012-08-02 14:13:38 +0000

    Tag correspondiente a la versión anterior a las mejoras sobre lectura de jars.

--- aether-core/src/main/java/com/tesis/aether/core/services/CloudServiceConstants.java
+++
@@ -1,7 +0,0 @@
-package com.tesis.aether.core.services;
-
-public class CloudServiceConstants {
-
-	public static final String STORAGE_KIND = "storage";
-
-}
--- aether-core/src/main/java/com/tesis/aether/core/services/storage/object/constants/StorageObjectConstants.java
+++
@@ -1,9 +0,0 @@
-package com.tesis.aether.core.services.storage.object.constants;
-
-public class StorageObjectConstants {
-
-	public static final String FILE_TYPE = "file";
-	public static final String DIRECTORY_TYPE = "directory";
-	public static final String CONTAINER_TYPE = "container";
-
-}
